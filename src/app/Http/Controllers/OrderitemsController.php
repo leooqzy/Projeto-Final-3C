0d294613c89f3650642fa293d27e93c4ae085d03
leooqzy <lemes7848@gmail.com>
2025-03-27 09:04:30 -0300

    Adicionando as Models e Migrations

---
+++ src/app/Http/Controllers/OrderitemsController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Orderitems;
+use Illuminate\Http\Request;
+
+class OrderitemsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Orderitems $orderitems)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Orderitems $orderitems)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Orderitems $orderitems)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Orderitems $orderitems)
+    {
+        //
+    }
+}
