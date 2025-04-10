1eea4100e535d68e623a93e1562b7b5593235daf
leooqzy <lemes7848@gmail.com>
2025-04-10 11:17:41 -0300

    Atualizando endereços

--- src/app/Http/Controllers/AddressesController.php
+++ src/app/Http/Controllers/AddressesController.php
@@ -18,27 +18,6 @@ class AddressesController extends Controller
         return response()->json($addresses, 200);
     }
 
-    public function UpdateAddress(Addresses $addresses)
-    {
-        $user = auth()->user();
-
-        if ($user->id !== $addresses->user_id) {
-            return response()->json([
-                'message' => 'You are not authorized to update this address',
-            ], 401);
-        }
-
-        $validated = $request->validate([
-            'street' => 'required|string|max:255',
-            'number' => 'required|integer|max:255',
-            'zip' => 'required|string|max:255',
-            'city' => 'required|string|max:255',
-            'state' => 'required|string|max:255',
-            'country' => 'required|string|max:255',
-        ]);
-
-        $addresses->update($validated);
-
     public function create(Request $request)
     {
         $validated = $request->validate([
@@ -77,5 +56,23 @@ class AddressesController extends Controller
         ], 200);
     }
 
+    public function update(Request $request, Addresses $addresses)
+    {
+        $validated = $request->validate([
+            'street' => 'required|string|max:255',
+            'number' => 'required|integer|max:255',
+            'zip' => 'required|string|max:255',
+            'city' => 'required|string|max:255',
+            'state' => 'required|string|max:255',
+            'country' => 'required|string|max:255',
+        ]);
+
+        $addresses->update($validated);
+
+        return response()->json([
+            'message' => 'Address updated successfully',
+            'addresses' => $addresses,
+        ], 200);
+    }
 
 }
